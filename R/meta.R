ec9f4a71c1973318622b0a70984a3363becd30e4
Kelsy Cain <kcain97@uw.edu>
2020-10-08 15:27:37 -0700

    removed green since not always able to normalize to beads

--- R/meta.R
+++ R/meta.R
@@ -4,7 +4,6 @@
 #' @param cruise cruise name, if any (otherwise NA).
 #' @param cruise_nickname cruise nickname name, if any (otherwise NA).
 #' @param project cruise name, if any (otherwise NA).
-#' @param unstained Sample stain status (TRUE/FALSE).
 #' @param version Version of the dataset.
 #' @return None
 #' @examples
@@ -12,7 +11,7 @@
 #' csv_convert(db, meta, path)
 #' }
 #' @export
-cmap_convert<- function(data, cruise, cruise_nickname, project, unstained, version = "v1.0") {
+cmap_convert<- function(data, cruise, cruise_nickname, project, version = "v1.0") {
 
   ## FORMAT DATA
 
@@ -40,10 +39,8 @@ cmap_convert<- function(data, cruise, cruise_nickname, project, unstained, versi
   # Pivot data from long format to wide; split population data for each variable (value) column
   data.pivot <- data %>%
         pivot_wider(names_from = population,
-                    values_from = c(count, scatter, red, orange, green, abundance, cell_diameter, carbon_content, biomass))
-if(unstained == TRUE){
-    data.pivot <- select(data.pivot, !starts_with("green"))
-  }
+                    values_from = c(count, scatter, red, orange, abundance, cell_diameter, carbon_content, biomass))
+# if(unstained == TRUE){data.pivot <- select(data.pivot, !starts_with("green"))}
 
   ## Format population data
   # master list of possible population names; add to as needed
@@ -70,7 +67,7 @@ if(unstained == TRUE){
             str_sort(str_subset(var_cols, "scatter_")),
             str_sort(str_subset(var_cols, "red_")),
             str_sort(str_subset(var_cols, "orange_")),
-            str_sort(str_subset(var_cols, "green_")),
+            #str_sort(str_subset(var_cols, "green_")),
             "volume",
             str_sort(str_subset(var_cols, "abundance_")),
             str_sort(str_subset(var_cols, "cell_diameter_")),
@@ -90,7 +87,7 @@ if(unstained == TRUE){
   long_scatter <- c()
   long_red <- c()
   long_orange <- c()
-  long_green <- c()
+  #long_green <- c()
   long_abundance <- c()
   long_cell_diameter <- c()
   #long_diameter_lwr <- c()
@@ -105,7 +102,7 @@ if(unstained == TRUE){
   comment_scatter <- c()
   comment_red <- c()
   comment_orange <- c()
-  comment_green <- c()
+  #comment_green <- c()
   comment_abundance <- c()
   comment_cell_diameter <- c()
   #comment_diameter_lwr <- c()
@@ -120,7 +117,7 @@ if(unstained == TRUE){
   keyword_scatter <- c()
   keyword_red <- c()
   keyword_orange <- c()
-  keyword_green <- c()
+  #keyword_green <- c()
   keyword_abundance <- c()
   keyword_cell_diameter <- c()
   #keyword_diameter_lwr <- c()
@@ -146,7 +143,7 @@ if(unstained == TRUE){
     long_scatter[[i]] <- paste0("forward angle light scatter of ", pop[i], "-like particles, normalized to 1 micron beads (proxy of cell diameter)")
     long_red[[i]] <- paste0("red fluorescence of ", pop[i], "-like particles, normalized to 1 micron beads (proxy of chlorophyll content)")
     long_orange[[i]] <- paste0("orange fluorescence of ", pop[i], "-like particles normalized to 1 micron beads (proxy of phycoerythrin content)")
-    long_green[[i]] <- paste0("green fluorescence of ", pop[i], "-like particles normalized to 1 micron beads (nucleic acid stain)")
+    #long_green[[i]] <- paste0("green fluorescence of ", pop[i], "-like particles normalized to 1 micron beads (nucleic acid stain)")
     long_abundance[[i]] <- paste0("cell abundance of ", pop[i], "-like particles")
     long_cell_diameter[[i]] <- paste0("equivalent spherical diameter of ", pop[i], "-like particles using ", RI)
     #long_diameter_lwr[[i]] <- paste(pop[i], "equivalent spherical diameter using high refractive index")
@@ -161,7 +158,7 @@ if(unstained == TRUE){
     comment_scatter[[i]] <- paste(pop[i], "light scatter collected using a 457-50 bandpass filter")
     comment_red[[i]] <- paste(pop[i], "red fluorescence collected using a 692-40 bandpass filter")
     comment_orange[[i]] <- paste(pop[i], "orange fluorescence collected using a 572-27 bandpass filter")
-    comment_green[[i]] <- paste(pop[i], "green fluorescence collected using a 530-40 bandpass filter")
+    #comment_green[[i]] <- paste(pop[i], "green fluorescence collected using a 530-40 bandpass filter")
     comment_abundance[[i]] <- paste(pop[i], "cell abundance, number of particles divided by the volume of sample, see https://github.com/fribalet/FCSplankton for more details")
     comment_cell_diameter[[i]] <- paste(pop[i], "cell diameter based on Mie theory using an index of refraction of", IR, "for phytoplankton and 1.337 for seawater, see https://github.com/seaflow-uw/fsc-size-calibration for more details")
     #comment_diameter_lwr[[i]] <- paste(pop[i], "cell diameter based on Mie theory using an index of refraction of 1.41 for phytoplankton and 1.337 for seawater, see https://github.com/seaflow-uw/fsc-size-calibration for more details")
@@ -176,7 +173,7 @@ if(unstained == TRUE){
     keyword_scatter[[i]] <- paste(pop[i], "forward angle light scatter, FSC, FALS,", core)
     keyword_red[[i]] <- paste(pop[i], "red fluorescence, chlorophyll,", core)
     keyword_orange[[i]] <- paste(pop[i], "orange fluorescence, phycoerythrin,", core)
-    keyword_green[[i]] <- paste(pop[i], "green fluorescence, SYBR Green I, nucleic acid stain,", core)
+    #keyword_green[[i]] <- paste(pop[i], "green fluorescence, SYBR Green I, nucleic acid stain,", core)
     keyword_abundance[[i]] <- paste(pop[i],"abundance, cell concentration, cell count, cell abundance,", core)
     keyword_cell_diameter[[i]] <- paste(pop[i],"size, diameter, ESD,", core)
     #keyword_diameter_lwr[[i]] <- paste(pop[i],"size, diameter, ESD,", core)
@@ -194,7 +191,7 @@ if(unstained == TRUE){
                      long_scatter,
                      long_red,
                      long_orange,
-                     long_green,
+                     #long_green,
                      "volume measured by pressure sensor",
                      long_abundance,
                      long_cell_diameter,
@@ -214,7 +211,7 @@ if(unstained == TRUE){
                    comment_scatter,
                    comment_red,
                    comment_orange,
-                   comment_green,
+                   #comment_green,
                    "volume measured by pressure sensor",
                    comment_abundance,
                    comment_cell_diameter,
@@ -230,7 +227,7 @@ if(unstained == TRUE){
                    "DNA stain (0 = unstained sample; 1 = stained sample)")
 
   var_unit <- c("",
-                rep("",length(pop)*5),
+                rep("",length(pop)*4),
                 "microliter",
                 rep("cells per microliter",length(pop)),
                 rep("micrometer", length(pop)),
@@ -242,21 +239,21 @@ if(unstained == TRUE){
   var_sensor <- rep("Flow cytometry",length(var_data))
 
   var_discipline <- c("",
-                     rep("Biology",length(pop)*5),
+                     rep("Biology",length(pop)*4),
                      "",
                      rep("Biology",length(pop)),
                      rep("Biology, Biogeochemistry", length(pop)*3),
                      "",
                      "")
 
-  visualize <- c(0, rep(1,length(pop)*5), 0, rep(1,length(pop)*4), 0, 0)
+  visualize <- c(0, rep(1,length(pop)*4), 0, rep(1,length(pop)*4), 0, 0)
 
   var_keywords <- c(paste("file,", core),
         keyword_count,
         keyword_scatter,
         keyword_red,
         keyword_orange,
-        keyword_green,
+        #keyword_green,
         paste("volume, vol,", core),
         keyword_abundance,
         keyword_cell_diameter,
@@ -271,37 +268,37 @@ if(unstained == TRUE){
         paste("flag,", core),
         paste("DNA stain, SYBR stain,", core))
 
-  if(unstained == TRUE){
-    var_data <- var_data[lapply(var_data,function(x) length(grep("green",x,value=FALSE))) == 0]
-
-    var_long_name <- var_long_name[lapply(var_long_name,function(x) length(grep("green",x,value=FALSE))) == 0]
-
-    var_comment <- var_comment[lapply(var_comment,function(x) length(grep("green",x,value=FALSE))) == 0]
-
-    var_unit <- c("",
-                  rep("",length(pop)*4),
-                  "microliter",
-                  rep("cells per microliter",length(pop)),
-                  rep("micrometer", length(pop)),
-                  rep("picogram carbon per cell", length(pop)),
-                  rep("microgram carbon per liter", length(pop)),
-                  "",
-                  "")
-
-    var_sensor <- rep("Flow cytometry",length(var_data))
-
-    var_discipline <- c("",
-                       rep("Biology",length(pop)*4),
-                       "",
-                       rep("Biology",length(pop)),
-                       rep("Biology, Biogeochemistry", length(pop)*3),
-                       "",
-                       "")
-
-    visualize <- c(0, rep(1,length(pop)*4), 0, rep(1,length(pop)*4), 0, 0)
-
-    var_keywords <- var_keywords[lapply(var_keywords,function(x) length(grep("green",x,value=FALSE))) == 0]
-  }
+#  if(unstained == TRUE){
+#    var_data <- var_data[lapply(var_data,function(x) length(grep("green",x,value=FALSE))) == 0]
+#
+#    var_long_name <- var_long_name[lapply(var_long_name,function(x) length(grep("green",x,value=FALSE))) == 0]
+#
+#    var_comment <- var_comment[lapply(var_comment,function(x) length(grep("green",x,value=FALSE))) == 0]
+#
+#    var_unit <- c("",
+#                  rep("",length(pop)*4),
+#                  "microliter",
+#                  rep("cells per microliter",length(pop)),
+#                  rep("micrometer", length(pop)),
+#                  rep("picogram carbon per cell", length(pop)),
+#                  rep("microgram carbon per liter", length(pop)),
+#                  "",
+#                  "")
+#
+#    var_sensor <- rep("Flow cytometry",length(var_data))
+#
+#    var_discipline <- c("",
+#                       rep("Biology",length(pop)*4),
+#                       "",
+#                       rep("Biology",length(pop)),
+#                       rep("Biology, Biogeochemistry", length(pop)*3),
+#                       "",
+#                       "")
+#
+#    visualize <- c(0, rep(1,length(pop)*4), 0, rep(1,length(pop)*4), 0, 0)
+#
+#    var_keywords <- var_keywords[lapply(var_keywords,function(x) length(grep("green",x,value=FALSE))) == 0]
+#  }
 
 
   # add custom column to metadata
